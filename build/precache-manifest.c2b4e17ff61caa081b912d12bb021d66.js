4a7607964b1468d9d65c6b83711d96da510a6ca1
NVIERAINFO <info.nviera@gmail.com>
2020-09-02 22:14:23 +0530

    build folder added

---
+++ build/precache-manifest.c2b4e17ff61caa081b912d12bb021d66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f7ed7d807a613a96c5623158a6d0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4ae81fa5ee87dd1b6f",
+    "url": "/static/css/main.85a4fc9e.chunk.css"
+  },
+  {
+    "revision": "7effc27a0e44ef06a61b",
+    "url": "/static/js/2.e3dbb87e.chunk.js"
+  },
+  {
+    "revision": "7f4ae81fa5ee87dd1b6f",
+    "url": "/static/js/main.2e44d92d.chunk.js"
+  },
+  {
+    "revision": "cfd6c8bb52d7e23e7a8d",
+    "url": "/static/js/runtime~main.ff2bed81.js"
+  }
+]);
